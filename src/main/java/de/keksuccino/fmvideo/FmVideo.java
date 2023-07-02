276a0da0034459b8ce7b60e4bff6b1d3c659c8da
SrRapero720 <SrRapero720@hotmail.com>
2023-07-02 11:58:53 -0600

    Updated mod id

--- src/main/java/de/keksuccino/fmvideo/FmVideo.java
+++ src/main/java/de/keksuccino/fmvideo/FmVideo.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod("fmextension_video")
+@Mod("fancymenu_video")
 public class FmVideo {
     public static final String VERSION = "1.0.0";
     public static final Logger LOGGER = LoggerFactory.getLogger(FmVideo.class);
